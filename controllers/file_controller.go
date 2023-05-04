f9ba3b0cf65ebcc8ec390024ecb8271eff48370b
TalentedSunday <sunasunday111@gmail.com>
2023-05-04 11:06:15 +0500

    finalize ipfs endpoint

--- controllers/file_controller.go
+++ controllers/file_controller.go
@@ -1,50 +1,50 @@
 package controllers
 
-import (
-	"context"
-	"fmt"
-	"io/ioutil"
-	"net/http"
-	middlewares "sodality/handlers"
-	"sodality/ipfs"
-	"sodality/models"
+// import (
+// 	"context"
+// 	"fmt"
+// 	"io/ioutil"
+// 	"net/http"
+// 	middlewares "sodality/handlers"
+// 	"sodality/ipfs"
+// 	"sodality/models"
 
-	iface "github.com/ipfs/interface-go-ipfs-core"
-)
+// 	iface "github.com/ipfs/interface-go-ipfs-core"
+// )
 
-var Session iface.CoreAPI
+// var Session iface.CoreAPI
 
-func init() {
-	Session = ipfs.StartingIPFS(context.Background())
-	ipfs.ConnectPeers(context.Background(), Session)
-}
+// func init() {
+// 	Session = ipfs.StartingIPFS(context.Background())
+// 	ipfs.ConnectPeers(context.Background(), Session)
+// }
 
-var UploadFile = http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+// var UploadFile = http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
 
-	file, header, err := r.FormFile("file")
-	if err != nil {
-		middlewares.ErrorResponse("error uploading file:"+err.Error(), rw)
-		return
-	}
-	defer file.Close()
+// 	file, header, err := r.FormFile("file")
+// 	if err != nil {
+// 		middlewares.ErrorResponse("error uploading file:"+err.Error(), rw)
+// 		return
+// 	}
+// 	defer file.Close()
 
-	data, err := ioutil.ReadAll(file)
-	if err != nil {
-		middlewares.ServerErrResponse(err.Error(), rw)
-		return
-	}
+// 	data, err := ioutil.ReadAll(file)
+// 	if err != nil {
+// 		middlewares.ServerErrResponse(err.Error(), rw)
+// 		return
+// 	}
 
-	path, err := ipfs.AddFilePath(data, r.Context(), Session)
-	if err != nil {
-		middlewares.ServerErrResponse(err.Error(), rw)
-		return
-	}
+// 	path, err := ipfs.AddFilePath(data, r.Context(), Session)
+// 	if err != nil {
+// 		middlewares.ServerErrResponse(err.Error(), rw)
+// 		return
+// 	}
 
-	var resp models.FileResp
+// 	var resp models.FileResp
 
-	resp.IpfsURL = fmt.Sprintf("https://ipfs.io/ipfs/%v", path.Cid())
-	resp.Filename = header.Filename
-	resp.FileSize = header.Size
+// 	resp.IpfsURL = fmt.Sprintf("https://ipfs.io/ipfs/%v", path.Cid())
+// 	resp.Filename = header.Filename
+// 	resp.FileSize = header.Size
 
-	middlewares.SuccessRespond(resp, rw)
-})
+// 	middlewares.SuccessRespond(resp, rw)
+// })
